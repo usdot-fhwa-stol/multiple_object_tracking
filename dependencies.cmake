fb7eb8c43f19df1d1d47f8d14c23894830ab26f6
Adam Morrissett <morrissettal2@vcu.edu>
2022-11-19 11:55:13 -0500

    Add DetectedObject class
    Add a DetectedObject class that is essentially a motion model container
    with a measurement time stamp. The CTRV motion model is the only one
    implemented, but others are on the way. It therefore makes sense to
    preemptively design the DetectedObject class as a class template.
    
    dependencies.cmake
    * add Boost.Container dependency for fixed-capacity vectors
    
    include/cooperative_perception/detected_object.hpp
    * add DetectedObject class implementation
    * add a DetectedObjectType type alias for std::variant. Each DetectedObject
      specialization will be a unique type, so we need an aggregate type to
      reason about DetectedObject generically.
    * add a DetectedObjectList type alias for boost::static_vector. This is
      a convenience type for library users.
    
    src/CMakeLists.txt
    * link to Boost.Container library
    
    test/CMakeLists.txt
    * add detected object test file to target sources
    
    test/test_detected_object.cpp
    * add default construction unit test for CTRV DetectedObject
      specialization
    * add custom construction unit test for CTRV DetectedObject
      specialization

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,3 +1,4 @@
+find_package(Boost REQUIRED COMPONENTS container)
 find_package(units REQUIRED)
 
 if(cooperative_perception_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
---
+++ test/test_detected_object.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <cooperative_perception/detected_object.hpp>
+#include <cooperative_perception/ctrv_model.hpp>
+#include <units.h>
+
+struct CtrvCovariance
+{
+};
+
+TEST(TestDetectedObject, CtrvObjectDefaultConstruction)
+{
+  auto object = cooperative_perception::DetectedObject<cooperative_perception::CtrvState, CtrvCovariance>{};
+
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.timestamp), 0.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.position_x), 0.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.position_y), 0.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.velocity), 0.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.yaw), 0.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.yaw_rate), 0.0);
+}
+
+TEST(TestDetectedObject, CtrvObjectCustomConstruction)
+{
+  using namespace units::literals;
+
+  // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
+  auto object = cooperative_perception::DetectedObject<cooperative_perception::CtrvState, CtrvCovariance>{
+    units::time::second_t{ 1 }, cooperative_perception::CtrvState{ 1_m, 2_m, 3_mps, 4_rad, 5_rad_per_s }
+  };
+  // NOLINTEND(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
+
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.timestamp), 1.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.position_x), 1.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.position_y), 2.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.velocity), 3.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.yaw), 4.0);
+  EXPECT_DOUBLE_EQ(units::unit_cast<double>(object.state.yaw_rate), 5.0);
+}
