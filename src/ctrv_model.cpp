50ba5a6bf7045b07231041fff61e10ed500a006a
Adam Morrissett <morrissettal2@vcu.edu>
2022-11-25 19:19:50 -0500

    Clean up CTRV nextState implementations
    The previous implementations were overly verbose, diminishing
    readability.
    
    src/ctrv_model.cpp
    * clean up deterministic nextState implementation
    * clean up stochastic nextState implementation

--- src/ctrv_model.cpp
+++ src/ctrv_model.cpp
@@ -10,47 +10,41 @@ auto nextState(const CtrvState& state, units::time::second_t time_step) -> CtrvS
 {
   using namespace units::literals;
 
-  units::length::meter_t delta_pos_x;
-  units::length::meter_t delta_pos_y;
+  CtrvState next_state{ state };
+
+  const auto delta_yaw{ state.yaw_rate * time_step };
 
   if (utils::almostEqual(units::unit_cast<double>(state.yaw_rate), 0.0))
   {
-    delta_pos_x = state.velocity * units::math::cos(state.yaw) * time_step;
-    delta_pos_y = state.velocity * units::math::sin(state.yaw) * time_step;
+    next_state.position_x += state.velocity * units::math::cos(state.yaw) * time_step;
+    next_state.position_y += state.velocity * units::math::sin(state.yaw) * time_step;
   }
   else
   {
-    delta_pos_x = state.velocity / state.yaw_rate *
-                  (units::math::sin(state.yaw + state.yaw_rate * time_step) - units::math::sin(state.yaw)) * 1_rad;
-    delta_pos_y = state.velocity / state.yaw_rate *
-                  (-units::math::cos(state.yaw + state.yaw_rate * time_step) + units::math::cos(state.yaw)) * 1_rad;
+    const auto vel_over_yaw_rate{ state.velocity / state.yaw_rate };
+    next_state.position_x +=
+        vel_over_yaw_rate * (units::math::sin(state.yaw + delta_yaw) - units::math::sin(state.yaw)) * 1_rad;
+    next_state.position_y -=
+        vel_over_yaw_rate * (units::math::cos(state.yaw + delta_yaw) - units::math::cos(state.yaw)) * 1_rad;
   }
 
-  const auto delta_vel{ 0_mps };
-  const units::angle::radian_t delta_yaw{ state.yaw_rate * time_step };
-  const auto delta_yaw_rate{ 0_rad_per_s };
+  next_state.yaw += delta_yaw;
 
-  return CtrvState{ state.position_x + delta_pos_x, state.position_y + delta_pos_y, state.velocity + delta_vel,
-                    state.yaw + delta_yaw, state.yaw_rate + delta_yaw_rate };
+  return next_state;
 }
 
 auto nextState(const CtrvState& state, units::time::second_t time_step, const CtrvProcessNoise& noise) -> CtrvState
 {
   auto next_state{ nextState(state, time_step) };
 
-  const units::length::meter_t delta_x{ 1.0 / 2.0 * noise.linear_acceleration * units::math::cos(state.yaw) *
-                                        units::math::pow<2>(time_step) };
-  const units::length::meter_t delta_y{ 1.0 / 2.0 * noise.linear_acceleration * units::math::sin(state.yaw) *
-                                        units::math::pow<2>(time_step) };
-  const units::velocity::meters_per_second_t delta_velocity{ noise.linear_acceleration * time_step };
-  const units::angle::radian_t delta_yaw{ 1.0 / 2.0 * noise.angular_acceleration * units::math::pow<2>(time_step) };
-  const units::angular_velocity::radians_per_second_t delta_yaw_rate{ noise.angular_acceleration * time_step };
+  const auto time_step_sq{ units::math::pow<2>(time_step) };
+  constexpr auto one_half{ 1.0 / 2.0 };
 
-  next_state.position_x += delta_x;
-  next_state.position_y += delta_y;
-  next_state.velocity += delta_velocity;
-  next_state.yaw += delta_yaw;
-  next_state.yaw_rate += delta_yaw_rate;
+  next_state.position_x += one_half * noise.linear_acceleration * units::math::cos(state.yaw) * time_step_sq;
+  next_state.position_y += one_half * noise.linear_acceleration * units::math::sin(state.yaw) * time_step_sq;
+  next_state.velocity += noise.linear_acceleration * time_step;
+  next_state.yaw += one_half * noise.angular_acceleration * time_step_sq;
+  next_state.yaw_rate += noise.angular_acceleration * time_step;
 
   return next_state;
 }
