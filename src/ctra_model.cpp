400cade56b32bc991d42d5b2f65d12a3aaabb905
Nathan Reyes <reyespinr@vcu.edu>
2022-12-14 16:26:16 +0000

    added copyright disclosure to files

--- src/ctra_model.cpp
+++ src/ctra_model.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Leidos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <cmath>
 #include <units.h>
 #include "cooperative_perception/ctra_model.hpp"
@@ -6,26 +21,34 @@
 
 namespace cooperative_perception
 {
-
 auto nextState(const CtraState& state, units::time::second_t time_step) -> CtraState
 {
-    using namespace units::literals;
+  using namespace units::literals;
 
-    units::length::meter_t delta_pos_x;
-    units::length::meter_t delta_pos_y;
+  units::length::meter_t delta_pos_x;
+  units::length::meter_t delta_pos_y;
 
-    auto velocity_new = state.velocity + state.acceleration * time_step;
-    auto yaw_new =  state.yaw + state.yaw_rate * time_step;
+  auto velocity_new = state.velocity + state.acceleration * time_step;
+  auto yaw_new = state.yaw + state.yaw_rate * time_step;
 
-    delta_pos_x =   (1 / (state.yaw_rate * state.yaw_rate)) * 
-                    (velocity_new * state.yaw_rate * units::math::sin(yaw_new) + state.acceleration * units::math::cos(yaw_new) - 
-                    state.velocity * state.yaw_rate * units::math::sin(state.yaw) - state.acceleration * units::math::cos(state.yaw));
+  delta_pos_x =
+      (1 / (state.yaw_rate * state.yaw_rate)) *
+      (velocity_new * state.yaw_rate * units::math::sin(yaw_new) + state.acceleration * units::math::cos(yaw_new) -
+       state.velocity * state.yaw_rate * units::math::sin(state.yaw) -
+       state.acceleration * units::math::cos(state.yaw));
 
-    delta_pos_y =   (1 / (state.yaw_rate * state.yaw_rate)) * 
-                    (-velocity_new * state.yaw_rate * units::math::cos(yaw_new) + state.acceleration * units::math::sin(yaw_new) + 
-                    state.velocity * state.yaw_rate * units::math::cos(state.yaw) - state.acceleration * units::math::sin(state.yaw));
+  delta_pos_y =
+      (1 / (state.yaw_rate * state.yaw_rate)) *
+      (-velocity_new * state.yaw_rate * units::math::cos(yaw_new) + state.acceleration * units::math::sin(yaw_new) +
+       state.velocity * state.yaw_rate * units::math::cos(state.yaw) -
+       state.acceleration * units::math::sin(state.yaw));
 
-    return CtraState{ state.position_x + delta_pos_x, state.position_y + delta_pos_y, velocity_new, yaw_new, state.yaw_rate, state.acceleration};
+  return CtraState{ state.position_x + delta_pos_x,
+                    state.position_y + delta_pos_y,
+                    velocity_new,
+                    yaw_new,
+                    state.yaw_rate,
+                    state.acceleration };
 }
 
 }  // namespace cooperative_perception
