0adebd9c2d0990191387d30f6fe6bfb4f9bd5c59
Nathan Reyes <reyespinr@vcu.edu>
2023-04-28 18:47:36 +0000

    update copyright year for headers

--- test/test_host_object_alignment.cpp
+++ test/test_host_object_alignment.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Leidos
+ * Copyright 2023 Leidos
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
