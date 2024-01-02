9bc3f1e66a1bd519c0699e77e912c4d0ad66aa49
dev <paul.k.bourelly@leidos.com>
2024-01-02 18:27:16 -0500

    Updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,5 +10,37 @@ cmake --install build
 cd /
 rm -rf /tmp/eigen
 
+# Install dlib
+git clone --depth 1 --branch v19.24.2 https://github.com/davisking/dlib.git /tmp/dlib
+cd /tmp/dlib
+cmake -B build
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTS=OFF \
+    -DDLIB_NO_GUI_SUPPORT=TRUE \
+    -DDLIB_JPEG_SUPPORT=FALSE \
+    -DDLIB_WEBP_SUPPORT=FALSE \
+    -DDLIB_PNG_SUPPORT=FALSE \
+    -DDLIB_GIF_SUPPORT=FALSE \
+    -DDLIB_USE_MKL_FFT=FALSE \
+    -DDLIB_USE_FFMPEG=FALSE \
+    -DDLIB_USE_CUDA=FALSE \
+    -DLIB_IN_PROJECT_BUILD=FALSE \
+    -DBUILD_DOCS=OFF
+cmake --build build
+cmake --install build
+cd /
+rm -rf /tmp/dlib
+
+# Install units library
+git clone --depth 1 --branch v2.3.3 https://github.com/nholthaus/units.git /tmp/units
+cd /tmp/units
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_DOCS=OFF
+cmake --build build
+cmake --install build
+cd /
+rm -rf /tmp/units
+
+
+
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libboost-container-dev libdlib-dev nlohmann-json3-dev libgtest-dev libgmock-dev libpng-dev libjpeg-dev libopenblas-dev
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libboost-container-dev nlohmann-json3-dev libgtest-dev libgmock-dev libpng-dev libjpeg-dev libopenblas-dev
