a204e416f44f639e0949906e43f992c1c492980c
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2024-01-05 11:26:37 -0500

    Remove CPM dependencies (#124)
    * Remove CPM dependencies
    
    + Added install_dependencies script
    + Moved testing scripts into test directory
    + Updated devcontainer setup to use install dependencies script
    
    * Update CI to use install dependencies
    
    * CI fix install_dependencies
    
    * Updates
    
    * Updates
    
    * Update
    
    * Updates
    
    * Updates
    
    * Update
    
    * Update ci to use carma-builds images
    
    * Fix yml syntax error
    
    * Install curl
    
    * install zip
    
    * Updates
    
    * Remove source calls to INIT_ENV
    
    * Fix github action triggers
    
    * Install gcovr through pip
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update
    
    * Updates
    
    * Update
    
    * Update gcovr call
    
    * Updates
    
    * Update gcovr
    
    * Update path for generating coverage report
    
    * Add check for testing option and include gtest version

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,45 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-set(CPM_DOWNLOAD_VERSION  0.38.2)
-include(cmake/get_cpm.cmake)
-
-CPMAddPackage(NAME units
-  GITHUB_REPOSITORY nholthaus/units
-  GIT_TAG v2.3.3
-  OPTIONS
-    "BUILD_TESTS FALSE"
-    "BUILD_DOCS FALSE"
-)
-
+find_package(units REQUIRED)
 find_package(Eigen3 REQUIRED)
-
-CPMAddPackage(NAME dlib
-  GITHUB_REPOSITORY davisking/dlib
-  GIT_TAG v19.24.2
-  OPTIONS
-    "DLIB_NO_GUI_SUPPORT TRUE"
-    "DLIB_JPEG_SUPPORT FALSE"
-    "DLIB_WEBP_SUPPORT FALSE"
-    "DLIB_PNG_SUPPORT FALSE"
-    "DLIB_GIF_SUPPORT FALSE"
-    "DLIB_USE_MKL_FFT FALSE"
-    "DLIB_USE_FFMPEG FALSE"
-    "DLIB_USE_CUDA FALSE"
-    "DLIB_IN_PROJECT_BUILD FALSE"
-)
-
-CPMAddPackage(NAME nlohmann_json
-  GITHUB_REPOSITORY nlohmann/json
-  GIT_TAG v3.11.2
-  OPTIONS
-    "JSON_BuildTests FALSE"
-    "JSON_Install TRUE"
-)
-
+find_package(dlib REQUIRED)
+find_package(nlohmann_json REQUIRED)
 find_package(Boost REQUIRED COMPONENTS container)
-
 if(multiple_object_tracking_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
-  CPMAddPackage("gh:google/googletest#v1.14.0")
+  # Currently findGTest does not currently actually work well for version
+  # so if you have compilation issues ensure you have the correct version
+  # installed
+  find_package(GTest 1.14.0 REQUIRED)
 endif()
