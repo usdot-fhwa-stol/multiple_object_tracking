d708b2d04d7adc712dccc7a3abb85a43994a9ddf
Adam Morrissett <adam.l.morrissett@leidos.com>
2024-01-08 15:19:06 -0500

    #134 Update GTest find_package call
    Remove version requirement and documenting comment because they are no
    longer necessary.

--- dependencies.cmake
+++ dependencies.cmake
@@ -17,8 +17,5 @@ find_package(dlib REQUIRED)
 find_package(nlohmann_json REQUIRED)
 find_package(Boost REQUIRED COMPONENTS container)
 if(multiple_object_tracking_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
-  # Currently findGTest does not currently actually work well for version
-  # so if you have compilation issues ensure you have the correct version
-  # installed
-  find_package(GTest 1.14.0 REQUIRED)
+  find_package(GTest REQUIRED)
 endif()
